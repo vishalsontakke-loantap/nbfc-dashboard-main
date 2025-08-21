f982d8c3e91d427ca0d16df76f5484fc3354e94b
Vishal <vishal.sontakke@loantap.in>
2025-08-21 14:58:07 +0530

    feat: add new SVG images for loan products and marketplace, and update SegmentsCard component for improved text display

--- src/components/NBFC/SegmentSelection.tsx
+++ src/components/NBFC/SegmentSelection.tsx
@@ -3,11 +3,12 @@ import { Card, CardContent } from "@/components/ui/card";
 import { X } from "lucide-react";
 import SegmentsCard from "../SegmentsCard";
 import { Link } from "react-router-dom";
+import PoolBuyoutInfoCard from "./PoolBuyoutInfoCard";
 
 const SegmentSelection = () => {
   return (
     <div className="max-w-[300dvh] mx-auto">
-      <div className="px-30 py-5">
+      <div className="">
         <Card className="scale-95 p-5">
           <div className="flex justify-between items-center">
             <h1 className="text-xl font-bold">
@@ -20,27 +21,46 @@ const SegmentSelection = () => {
             Seamlessly onboard for Co-Lending or Pool Buyout and start your
             journey
           </p>
-          <CardContent className="grid grid-cols-2 gap-4 mt-4">
-            <Link to={"/nbfc/nbfc-form"}>
+          <CardContent className="grid grid-cols-4 gap-4 mt-4">
+          <Link to={"/nbfc/nbfc-form"}>
               <SegmentsCard
-                imgSrc="/images/co-lending_segment.svg"
-                imgAlt="Co-Lending Segment"
-                heading="Co-Lending"
-                subtext="Partner seamlessly to co-lend and scale your lending business."
-              />
-            </Link>
-
-            <Link to={"/nbfc/nbfc-form"}>
-              <SegmentsCard
-                imgSrc="/images/pool_buyout_segment.svg"
+                imgSrc="/images/Pool.svg"
                 imgAlt="Pool Buyout Segment"
                 badge="Beta"
                 heading="Pool Buyout"
                 subtext="Acquire high-quality loan pools with ease and efficiency."
               />
             </Link>
+            <Link to={"/nbfc/nbfc-form"}>
+              <SegmentsCard
+                imgSrc="/images/co-landing.svg"
+                imgAlt="Co-Lending Segment"
+                heading="Co-Lending"
+                subtext="Partner seamlessly to co-lend and scale your lending business."
+              />
+            </Link>
+            <Link to={"/nbfc/nbfc-form"}>
+              <SegmentsCard
+                imgSrc="/images/loan_product.svg"
+                imgAlt="Pool Buyout Segment"
+                // badge="Beta"
+                heading="Loan Product Management"
+                subtext="Centralized platform to create, configure & manage loan offerings."
+              />
+            </Link>
+            <Link to={"/nbfc/nbfc-form"}>
+              <SegmentsCard
+                imgSrc="/images/loan_market_place.svg"
+                imgAlt="Loan Marketplace"
+                badge="Beta"
+                heading="Pool Buyout"
+                subtext="Digital marketplace to source, distribute, and track loan leads."
+              />
+            </Link>
           </CardContent>
         </Card>
+
+        <PoolBuyoutInfoCard/>
       </div>
     </div>
   );
--- src/components/SegmentsCard.tsx
+++ src/components/SegmentsCard.tsx
@@ -15,12 +15,14 @@ const SegmentsCard = ({
         <img src={imgSrc} alt={imgAlt} className={"w-full"} />
         <span className="flex flex-col p-3">
           <span className="flex space-x-2 items-center">
-          <h2 className="font-bold text-lg">{heading}</h2>
+          <h3 className="font-bold text-sm">{heading}</h3>
             {badge && (
               <Badge variant={'destructive'} className="text-xs font-semibold">{badge}</Badge>
             )}
           </span>
-          <p className="text-sm text-muted-foreground mt-2">{subtext}</p>
+          <p className="text-xs text-muted-foreground mt-2 break-words whitespace-normal">
+        {subtext}
+      </p>
         </span>
       </div>
     </div>
